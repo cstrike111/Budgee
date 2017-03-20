7d2b7b91677af74e38b974b4a33c2da3fb94bda5
Steve <cstrike111@163.com>
2017-03-20 00:01:33 +0000

    a

---
+++ app/src/main/java/team3/budgee/activity/AdvancedSetup.java
@@ -0,0 +1,16 @@
+package team3.budgee.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import team3.budgee.R;
+
+public class AdvancedSetup extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_advanced_setup);
+    }
+
+}
---
+++ app/src/main/java/team3/budgee/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package team3.budgee.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import team3.budgee.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
